283710c428a04a259508de5a6cb4ba9415ae6b23
Mark O'Connor <mark@myspotontheweb.com>
2024-04-06 01:33:06 +0100

    Fix applicationset

--- bootstrap/k3d/run.sh
+++ bootstrap/k3d/run.sh
@@ -21,4 +21,3 @@ kubectl --context $PILOT_CONTEXT create ingress argocd  --class=traefik --rule=/
 # Print login details
 #
 kubectl --context $PILOT_CONTEXT get secret argocd-initial-admin-secret -n argocd  -ogo-template='{{printf "\nurl: http://localhost:8080/argocd\nuser: admin\npass: %s\n" (.data.password|base64decode)}}'
-kubectl --context $PILOT_CONTEXT get secret argocd-initial-admin-secret -n argocd  -ogo-template='{{printf "\nargocd login http://localhost:8080/argocd --username admin --password %s\n" (.data.password|base64decode)}}'
